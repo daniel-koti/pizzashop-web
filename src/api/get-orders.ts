3975aa8c300c2e1262a78280a1193fe7917e2778
Daniel Koti <danielkoti.moniz@gmail.com>
2024-08-28 20:47:18 +0100

    feat: fetch orders

---
+++ src/api/get-orders.ts
@@ -0,0 +1,35 @@
+import { api } from '@/lib/axios'
+
+type OrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+export interface Order {
+  orderId: string
+  createdAt: string
+  status: OrderStatus
+  customerName: string
+  total: number
+}
+
+export interface GetOrdersResponse {
+  orders: Order[]
+  meta: {
+    pageIndex: number
+    perPage: number
+    totalCount: number
+  }
+}
+
+export async function getOrders() {
+  const response = await api.get<GetOrdersResponse>('/orders', {
+    params: {
+      pageIndex: 0,
+    },
+  })
+
+  return response.data
+}
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,12 +1,20 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { ArrowRight, Search, X } from 'lucide-react'
 
+import { Order } from '@/api/get-orders'
+import { OrderStatus } from '@/components/order-status'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
 
 import { OrderDetails } from './order-details'
 
-export function OrderTableRow() {
+interface OrderTableRowProps {
+  order: Order
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -22,17 +30,24 @@ export function OrderTableRow() {
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
-        827uir91jynlqb7
+        {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true,
+        })}
+      </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span className="font-medium text-muted-foreground">Pendente</span>
-        </div>
+        <OrderStatus status={order.status} />
       </TableCell>
-      <TableCell className="font-medium">Daniel Koti Moniz</TableCell>
-      <TableCell className="font-medium">132$</TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">
+        {order.total.toLocaleString('en', {
+          style: 'currency',
+          currency: 'USD',
+        })}
+      </TableCell>
 
       <TableCell>
         <Button variant="outline" size="xs">
