73c313cea87115175598a64babafda09fdab8817
Daniel Koti <danielkoti.moniz@gmail.com>
2024-08-29 14:25:31 +0100

    feat: automatic sign-out

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,35 @@
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // create event listener
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    // I have to clear my event listeners if everything is alright
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
