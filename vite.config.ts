791e0733beb5f1e1a1027be0f4497ab2fbf6c60d
Daniel Koti <danielkoti.moniz@gmail.com>
2024-09-02 19:28:45 +0100

    Config tests 🧪
    Add Vitest
    Add Testing Library
    Add Happy Dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+/// <reference types="vitest" />
 import path from 'node:path'
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +11,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'edge-runtime',
   },
 })
